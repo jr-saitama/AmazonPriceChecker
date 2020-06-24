5930f61b8b0eaa932d72abea0e31eee6f6c29843
tejas <tejastejumanoj@gmail.com>
2020-06-24 23:09:07 +0530

    minor changes

--- AmazonPriceChecker.py
+++ AmazonPriceChecker.py
@@ -6,6 +6,7 @@ import socket
 
 def get_data():
         url_list =  []
+        email= None
         """ url_list = []
         user_input = int(input('enter the number of items : '))
         print("\nCopy and paste the urls of the items you need one after the other\n\n")
@@ -18,15 +19,16 @@ def get_data():
             read_lines = f.readlines()
 
         user_agent = read_lines[0].strip('\n')
-        url_list.extend(read_lines[1:])
+        email = read_lines[1].strip('\n')
+        url_list.extend(read_lines[2:])
         
-        # url_list = ["https://www.amazon.in/HP-Pentium-14-inch-Windows-14q-cs0018TU/dp/B07THRK915/"]    
+            
         no_url = len(url_list)
         price_dict ={"item_name":[], "price":[]}
         for i in range(no_url):
             url = url_list[i]
             if(url[12]=='a'):
-                # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"
+                
                 headers = {"User-Agent" : user_agent}
                 page = requests.get(url, headers=headers)
 
@@ -43,7 +45,7 @@ def get_data():
                 print("price : " + str(price_dict['price'][i]) + "\n")
 
             """ elif(url[12]=='f'):
-                headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"}
+                headers = {"User-Agent" : user_agent}
                 page = requests.get(url, headers=headers)
 
                 soup = BeautifulSoup(page.content, "html.parser")
@@ -72,7 +74,7 @@ def get_data():
 
         msg = f"Subject:{subject}\n\n{body}"
 
-        server.sendmail("myfirstcompanyinengineering@gmail.com", "naveenkumarburugupally@gmail.com",msg)
+        server.sendmail("myfirstcompanyinengineering@gmail.com", email ,msg)
 
         print('email is sent')
 
